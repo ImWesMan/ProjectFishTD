f06cdb0936caf00c81221a4793a8718693d3f780
ImWesMan <66706588+ImWesMan@users.noreply.github.com>
2023-05-04 22:44:33 -0700

    Started round spawning scripts

---
+++ Assets/Fish/waveManager/fishSpawner.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fishSpawner : MonoBehaviour
+{
+    public GameObject basicFish;
+    public GameObject behemothFish;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
