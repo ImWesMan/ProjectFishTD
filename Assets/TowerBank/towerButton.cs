7944888019579db2da61ce6be9f3a8ea246d6b0a
ImWesMan <66706588+ImWesMan@users.noreply.github.com>
2023-05-11 13:39:23 -0700

    Edge case and tower bank functionality

--- Assets/TowerBank/towerButton.cs
+++ Assets/TowerBank/towerButton.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.EventSystems;
 public class towerButton : MonoBehaviour
 {
     public GameObject linkedTower;
@@ -13,6 +14,9 @@ public class towerButton : MonoBehaviour
     public bool isDragging;
     public bool isPressed;
     public bool inPreviewMode;
+    public bool leftTheBank = false;
+    public bool towerPlaced = false;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -29,9 +33,30 @@ public class towerButton : MonoBehaviour
         {
             gameObject.GetComponent<Button>().interactable = false;
         }
-        else
+        if(((towerPreview == null && !Input.GetMouseButton(0)) || (Input.GetMouseButtonUp(0) && towerPlaced)) && levelManager.GetComponent<levelManager>().money >= cost)
         {
             gameObject.GetComponent<Button>().interactable = true;
+        }
+
+        if (EventSystem.current.IsPointerOverGameObject() && inPreviewMode)
+        {
+            towerPreview.GetComponent<SpriteRenderer>().enabled = false;
+        }
+        if (!EventSystem.current.IsPointerOverGameObject() && inPreviewMode)
+        {
+             towerPreview.GetComponent<SpriteRenderer>().enabled = true;
+             leftTheBank = true;
+        }
+
+        if(leftTheBank && EventSystem.current.IsPointerOverGameObject() && inPreviewMode)
+        {
+            Debug.Log("Tower preview destroyed, Reason: left and re-entered bank");
+            Destroy(towerPreview);
+            isPressed = false;
+            isDragging = false;
+            inPreviewMode = false;
+            leftTheBank = false;
+            return;
         }
 
         if (isPressed || isDragging)
@@ -56,49 +81,63 @@ public class towerButton : MonoBehaviour
             // If the user clicks, place the real tower
             if (Input.GetMouseButtonDown(0) && !insideCollider && isPressed)
             {
+                Debug.Log("Tower preview destroyed, Reason: placed");
                 levelManager.GetComponent<levelManager>().subtractMoney(cost);
                 GameObject newTower = Instantiate(linkedTower, towerPreview.transform.position, Quaternion.identity);
                 Destroy(towerPreview);
                 isPressed = false;
                 inPreviewMode = false;
+                leftTheBank = false;
+                towerPlaced= true;
             }
             if (Input.GetMouseButtonDown(0) && insideCollider && isPressed)
             {
+                Debug.Log("Tower preview destroyed, Reason: placed in collider");
                 Destroy(towerPreview);
                 isPressed = false;
                 inPreviewMode = false;
+                leftTheBank = false;
             }
             if (Input.GetMouseButtonUp(0) && !insideCollider && isDragging)
             {
+                Debug.Log("Tower preview destroyed, Reason: placed");
                 levelManager.GetComponent<levelManager>().subtractMoney(cost);
                 GameObject newTower = Instantiate(linkedTower, towerPreview.transform.position, Quaternion.identity);
                 Destroy(towerPreview);
                 isDragging = false;
                 inPreviewMode = false;
+                leftTheBank= false;
+                towerPlaced = true;
             }
             if (Input.GetMouseButtonUp(0) && insideCollider && isDragging)
             {
+                Debug.Log("Tower preview destroyed, Reason: let go in collider");
                 Destroy(towerPreview);
                 isDragging = false;
                 inPreviewMode = false;
+                leftTheBank = false;
             }
         }
     }
 
     public void onPress()
     {
+        towerPlaced= false;
         if(levelManager.GetComponent<levelManager>().money < cost)
         {
+            Debug.Log("Not enough money");
             return;
         }
         if(inPreviewMode)
         {
+            Debug.Log("pressed button while in preview mode");
             Destroy(towerPreview);
             isPressed = false;
             inPreviewMode = false;
         }
         else
         {
+        Debug.Log("Preview made");
         inPreviewMode = true;
         towerPreview = Instantiate(linkedTower, new Vector3(999, 999, 0), Quaternion.identity);
         towerPreview.GetComponent<Tower>().enabled = false;
@@ -109,6 +148,7 @@ public class towerButton : MonoBehaviour
 
      public void onDrag()
     {
+        towerPlaced= false;
         if(levelManager.GetComponent<levelManager>().money < cost)
         {
             return;
